bc498c7fd6548792114a177f11461db19e2917b0
Lukas Lebovitz <lukas.lebo@hotmail.com>
2023-05-02 17:34:54 +0200

    make window prettier with title, icon and dark background

--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -1,4 +1,5 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -6,22 +7,44 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowPosition
+import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.application
+import model.board.King
+import model.board.Set
 import view.board.ChessBoard
 
 fun main() = application {
-    Window(onCloseRequest = ::exitApplication) {
+    val state = WindowState(
+        size = DpSize(1100.dp, 900.dp),
+        position = WindowPosition(Alignment.Center),
+    )
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "Chess",
+        icon = painterResource(King(Set.BLACK).asset),
+        state = state,
+    ) {
         App()
     }
 }
+
+val dark = Color(0xFF2B2B2B)
+val darkFaded = Color(0xFF323232)
 
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
-        Box(modifier = Modifier.fillMaxSize().padding(32.dp), contentAlignment = Alignment.BottomCenter) {
+        Box(
+            modifier = Modifier.fillMaxSize().background(dark).padding(32.dp),
+            contentAlignment = Alignment.BottomCenter
+        ) {
             ChessBoard()
         }
     }
